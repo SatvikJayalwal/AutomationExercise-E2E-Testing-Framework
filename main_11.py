fc245ed89c53a6e96b8f54b79ddd97acfeaa6c26
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2025-09-05 11:39:11 +0530

    updated main_11.py

--- main_11.py
+++ main_11.py
@@ -21,9 +21,30 @@ assert home_logo.is_displayed(), "HOME PAGE not displayed"
 driver.find_element(By.XPATH,"//a[@href='/view_cart']").click()
 
 # Scroll down to footer
-WebDriverWait(driver,10).until(
-    EC.visibility_of_element_located((By.XPATH,"//"))
-)
+subscription = driver.find_element(By.XPATH,"//div[@class='single-widget']/h2")
+driver.execute_script("arguments[0].scrollIntoView();", subscription)
+
 # Verify text 'SUBSCRIPTION'
+assert "SUBSCRIPTION"==subscription.text
+
 # Enter email address in input and click arrow button
+email = WebDriverWait(driver,10).until(
+    EC.element_to_be_clickable((By.XPATH,"//input[@id='susbscribe_email']"))
+)
+email.send_keys("satvikjayalwalips@gmail.com")
+
+arrow = WebDriverWait(driver,10).until(
+    EC.element_to_be_clickable((By.XPATH,"//button[@id='subscribe']"))
+)
+arrow.click()       
+
 # Verify success message 'You have been successfully subscribed!' is visible
+subscribe_success_msg = WebDriverWait(driver,10).until(
+    EC.visibility_of_element_located((By.XPATH,"//div[@class='alert-success alert']"))
+)
+
+assert subscribe_success_msg.is_displayed(), "YOU HAVE BEEN SUCCESSFULLY SUBSCRIBED not visible"
+
+# Quit driver
+time.sleep(2)
+driver.quit()
