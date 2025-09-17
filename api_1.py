99c83dc200a332f3470c47778e0764356a76aa99
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2025-09-17 17:11:07 +0530

    api_1.py

---
+++ api_1.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+# API URL
+url = "https://automationexercise.com/api/productsList"
+
+# Send GET request
+response = requests.get(url)
+
+# Validate Status Code
+status = response.status_code
+print(f"Status Code : {status}")
+assert status == 200, f"Expected 200(OK) but got {status}"
+
+# Parse JSON response
+data = response.json()
+
+# Print data 
+print(json.dumps(data,indent=2))
+
+# Validate 'products' key exits 
+assert "products" in data, "'products' key not found in response"
+
+# Validate product list is not empty 
+assert len(data['products']) > 0, "Product list is empty"
